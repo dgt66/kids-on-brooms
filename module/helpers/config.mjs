a47b304aa5e37f8fabf10df0d44b6be5bc225bc5
Douglas Tallach <dougnazareno@gmail.com>
2023-12-22 20:22:57 -0300

    feat: insert some feats of kids on broom

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -10,7 +10,13 @@ export const BOILERPLATE = {};
   "con": "BOILERPLATE.AbilityCon",
   "int": "BOILERPLATE.AbilityInt",
   "wis": "BOILERPLATE.AbilityWis",
-  "cha": "BOILERPLATE.AbilityCha"
+  "cha": "BOILERPLATE.AbilityCha",
+  "brains": "BOILERPLATE.AbilityBrains",
+  "brawn": "BOILERPLATE.AbilityBrawn",
+  "fight": "BOILERPLATE.AbilityFight",
+  "flight": "BOILERPLATE.AbilityFlight",
+  "charm": "BOILERPLATE.AbilityCharm",
+  "grit": "BOILERPLATE.AbilityGrit"
 };
 
 BOILERPLATE.abilityAbbreviations = {
@@ -19,5 +25,24 @@ BOILERPLATE.abilityAbbreviations = {
   "con": "BOILERPLATE.AbilityConAbbr",
   "int": "BOILERPLATE.AbilityIntAbbr",
   "wis": "BOILERPLATE.AbilityWisAbbr",
-  "cha": "BOILERPLATE.AbilityChaAbbr"
+  "cha": "BOILERPLATE.AbilityChaAbbr",
+  "brains": "BOILERPLATE.AbilityBrainsAbbr",
+  "brawn": "BOILERPLATE.AbilityBrawnAbbr",
+  "fight": "BOILERPLATE.AbilityFightAbbr",
+  "flight": "BOILERPLATE.AbilityFlightAbbr",
+  "charm": "BOILERPLATE.AbilityCharmAbbr",
+  "grit": "BOILERPLATE.AbilityGritAbbr"
+};
+
+BOILERPLATE.infos = {
+  "trope": "BOILERPLATE.infoTrope",
+  "age": "BOILERPLATE.infoAge",
+  "pronouns": "BOILERPLATE.infopronouns",
+  "fear": "BOILERPLATE.infoFear",
+  "motivation": "BOILERPLATE.infoMotivation",
+  "grade": "BOILERPLATE.infoGrade",
+  "animalFamiliar": "BOILERPLATE.infoAnimalFamiliar",
+  "adversity": "BOILERPLATE.infoAdversity",
+  "tropeQuestions": "BOILERPLATE.infoTropeQuestions",
+  "strengths": "BOILERPLATE.infoStrengths"
 };
